16325844c9fb3304563160c8bd46fbf8609be0d1
burakgunce <brk.gnc9@hotmail.com>
2024-04-16 23:39:51 +0300

    EventBus unit test layer has been added

---
+++ tests/BuildingBlocks/EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,124 @@
+using EventBus.Base.Abstraction;
+using EventBus.Base;
+using EventBus.UnitTest.Events.EventHandlers;
+using EventBus.UnitTest.Events.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Logging;
+using EventBus.Factory;
+
+namespace EventBus.UnitTest
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        private ServiceCollection services;
+
+        public EventBusTests()
+        {
+            services = new ServiceCollection();
+            services.AddLogging(configure => configure.AddConsole());
+        }
+
+        [TestMethod]
+        public void subscribe_event_on_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+
+
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+            //eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+        }
+
+        [TestMethod]
+        public void subscribe_event_on_azure_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetAzureConfig(), sp);
+            });
+
+
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+            //eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+
+            Task.Delay(2000).Wait();
+        }
+
+        [TestMethod]
+        public void send_message_to_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+
+
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Publish(new OrderCreatedIntegrationEvent(1));
+        }
+
+        [TestMethod]
+        public void send_message_to_azure_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetAzureConfig(), sp);
+            });
+
+
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Publish(new OrderCreatedIntegrationEvent(1));
+        }
+
+
+        private EventBusConfig GetAzureConfig()
+        {
+            return new EventBusConfig()
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "SellingMicroserviceTopicName",
+                EventBusType = EventBusType.AzureServiceBus,
+                EventNameSuffix = "IntegrationEvent",
+                EventBusConnectionString = "Endpoint=sb://sellingmicroservis.servicebus.windows.net/;SharedAccessKeyName=NewPolicyForYTVideos;SharedAccessKey=7sJghGWFOXaUaRblrbzOIIf4bQk6qkbTN/SEnKjXLpE="
+            };
+        }
+
+        private EventBusConfig GetRabbitMQConfig()
+        {
+            return new EventBusConfig()
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "SellingMicroserviceTopicName",
+                EventBusType = EventBusType.RabbitMQ,
+                EventNameSuffix = "IntegrationEvent",
+                //Connection = new ConnectionFactory()
+                //{ 
+                //    HostName = "localhost",
+                //    Port = 15672,
+                //    UserName = "guest",
+                //    Password = "guest"
+                //}
+            };
+        }
+    }
+}
