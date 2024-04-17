4fb1922bc3ff353490b85dfbca7c5d2643834093
burakgunce <brk.gnc9@hotmail.com>
2024-04-17 23:55:00 +0300

    Identity service has been prepared

--- src/Services/IdentityService/IdentityService.Api/Program.cs
+++ src/Services/IdentityService/IdentityService.Api/Program.cs
@@ -1,35 +1,70 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace IdentityService.Api
 {
     public class Program
     {
+        private static string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        private static IConfiguration configuration
+        {
+            get
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile($"Configurations/appsettings.json", optional: false)
+                    .AddJsonFile($"Configurations/appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
+        private static IConfiguration serilogConfiguration
+        {
+            get
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile($"Configurations/serilog.json", optional: false)
+                    .AddJsonFile($"Configurations/serilog.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
+        public static IWebHost BuildWebHost(IConfiguration configuration, string[] args)
+        {
+            return WebHost.CreateDefaultBuilder()
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateOnBuild = false;
+                    options.ValidateScopes = false;
+                })
+                .ConfigureAppConfiguration(i => i.AddConfiguration(configuration))
+                .UseStartup<Startup>()
+                .ConfigureLogging(i => i.ClearProviders())
+                .Build();
+        }
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var host = BuildWebHost(configuration, args);
 
-            // Add services to the container.
+            Log.Logger = new LoggerConfiguration()
+                .CreateLogger();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            Log.Logger.Information("Application is Running....");
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            host.Run();
         }
     }
 }
