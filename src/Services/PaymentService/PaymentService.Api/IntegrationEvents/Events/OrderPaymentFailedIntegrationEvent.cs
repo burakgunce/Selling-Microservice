a4f8712989d2626eb19c956967b22a40ce0e922e
burakgunce <brk.gnc9@hotmail.com>
2024-04-17 22:28:32 +0300

    payment service has been prepared

---
+++ src/Services/PaymentService/PaymentService.Api/IntegrationEvents/Events/OrderPaymentFailedIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using EventBus.Base.Events;
+
+namespace PaymentService.Api.IntegrationEvents.Events
+{
+    public class OrderPaymentFailedIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; }
+        public string ErrorMessage { get; }
+
+        public OrderPaymentFailedIntegrationEvent(Guid orderId, string errorMessage)
+        {
+            OrderId = orderId;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
--- src/Services/PaymentService/PaymentService.Api/Program.cs
+++ src/Services/PaymentService/PaymentService.Api/Program.cs
@@ -1,3 +1,8 @@
+using EventBus.Base.Abstraction;
+using EventBus.Base;
+using EventBus.Factory;
+using RabbitMQ.Client;
+
 namespace PaymentService.Api
 {
     public class Program
@@ -6,28 +11,63 @@ namespace PaymentService.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var env = builder.Environment;
 
+            // Configuration
+            builder.Configuration
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("Configurations/appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"Configurations/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            // Serilog Configuration
+            //Log.Logger = new LoggerConfiguration()
+                //.ReadFrom.Configuration(builder.Configuration)
+                //.CreateLogger();
+
+            // Services
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            //builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+            // Event Bus Configuration
+            builder.Services.AddSingleton<IEventBus>(sp =>
+            {
+                EventBusConfig config = new()
+                {
+                    ConnectionRetryCount = 5,
+                    EventNameSuffix = "IntegrationEvent",
+                    SubscriberClientAppName = "PaymentService",
+                    EventBusType = EventBusType.RabbitMQ,
+                    Connection = new ConnectionFactory()
+                    {
+                        HostName = "localhost",
+                        Port = 15672,
+                        UserName = "guest",
+                        Password = "guest"
+                    }
+                };
+
+                return EventBusFactory.Create(config, sp);
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            // Middleware
+            if (env.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentService.Api v1"));
             }
 
             app.UseHttpsRedirection();
-
+            app.UseRouting();
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            //Log.Information("Application is Running....");
 
             app.Run();
         }
