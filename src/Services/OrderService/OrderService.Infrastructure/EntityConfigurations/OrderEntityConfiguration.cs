b305ab7f03cd442083be8008ed99dc859ad226eb
burakgunce <brk.gnc9@hotmail.com>
2024-04-21 02:52:00 +0300

    infrastructure layer of order service has been written

---
+++ src/Services/OrderService/OrderService.Infrastructure/EntityConfigurations/OrderEntityConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Infrastructure.Context;
+using OrderService.Domain.AggregateModels.OrderAggregate;
+
+namespace OrderService.Infrastructure.EntityConfigurations
+{
+    public class OrderEntityConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("orders", OrderDbContext.DEFAULT_SCHEMA);
+
+            builder.HasKey(o => o.Id);
+            builder.Property(i => i.Id).ValueGeneratedOnAdd();
+
+            builder.Ignore(i => i.DomainEvents);
+
+
+            builder
+                .OwnsOne(o => o.Address, a =>
+                {
+                    a.WithOwner();
+                });
+
+
+            builder
+                .Property<int>("orderStatusId")
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .HasColumnName("OrderStatusId")
+                .IsRequired();
+
+
+            var navigation = builder.Metadata.FindNavigation(nameof(Order.OrderItems));
+
+            navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+
+            builder.HasOne(o => o.Buyer)
+                .WithMany()
+                .HasForeignKey(i => i.BuyerId);
+
+
+            builder.HasOne(o => o.OrderStatus)
+                .WithMany()
+                .HasForeignKey("orderStatusId");
+        }
+    }
+}
---
+++ src/Services/OrderService/OrderService.Infrastructure/EntityConfigurations/OrderStatusEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Infrastructure.Context;
+using OrderService.Domain.AggregateModels.OrderAggregate;
+
+namespace OrderService.Infrastructure.EntityConfigurations
+{
+    internal class OrderStatusEntityConfiguration : IEntityTypeConfiguration<OrderStatus>
+    {
+        public void Configure(EntityTypeBuilder<OrderStatus> builder)
+        {
+            builder.ToTable("orderstatus", OrderDbContext.DEFAULT_SCHEMA);
+
+            builder.HasKey(o => o.Id);
+            builder.Property(i => i.Id).ValueGeneratedOnAdd();
+
+            builder.Property(o => o.Id)
+                .HasDefaultValue(1)
+                .ValueGeneratedNever()
+                .IsRequired();
+
+            builder.Property(o => o.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
