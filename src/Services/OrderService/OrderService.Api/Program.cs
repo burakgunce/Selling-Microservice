1cc46dbda9e1c6b81d5e22b21caf73bbed2ed6ee
burakgunce <brk.gnc9@hotmail.com>
2024-04-21 17:18:26 +0300

    orders controller was updated and middlewares was updated

--- src/Services/OrderService/OrderService.Api/Program.cs
+++ src/Services/OrderService/OrderService.Api/Program.cs
@@ -1,35 +1,85 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrderService.Api.Extensions;
+using OrderService.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
 namespace OrderService.Api
 {
     public class Program
     {
+        private static string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        private static IConfiguration configuration
+        {
+            get
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile($"Configurations/appsettings.json", optional: false)
+                    .AddJsonFile($"Configurations/appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
+        private static IConfiguration serilogConfiguration
+        {
+            get
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile($"Configurations/serilog.json", optional: false)
+                    .AddJsonFile($"Configurations/serilog.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
+        public static IWebHost BuildWebHost(IConfiguration configuration, string[] args)
+        {
+            return WebHost.CreateDefaultBuilder()
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateOnBuild = false;
+                    options.ValidateScopes = false;
+                })
+                .ConfigureAppConfiguration(i => i.AddConfiguration(configuration))
+                .UseStartup<Startup>()
+                .ConfigureLogging(i => i.ClearProviders())
+                .UseSerilog()
+                .Build();
+        }
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var host = BuildWebHost(configuration, args);
 
-            // Add services to the container.
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(serilogConfiguration)
+                .CreateLogger();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            host.MigrateDbContext<OrderDbContext>((context, services) =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                var logger = services.GetService<ILogger<OrderDbContext>>();
 
-            app.UseHttpsRedirection();
+                var dbContextSeeder = new OrderDbContextSeed();
+                dbContextSeeder.SeedAsync(context, logger)
+                    .Wait();
+            });
 
-            app.UseAuthorization();
+            Log.Information("Application is Running....");
 
-
-            app.MapControllers();
-
-            app.Run();
+            host.Run();
         }
     }
 }
