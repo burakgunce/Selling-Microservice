0094ba92a0d8f0e20e83432cc768f6d60a56bd26
burakgunce <brk.gnc9@hotmail.com>
2024-04-22 03:03:54 +0300

    the services required to perform aggregation operations on apigateway were written and the basket page was prepared

---
+++ src/Clients/BlazorWebApp/WebApp/Application/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Application.Services.DTOs;
+using WebApp.Domain.Models.ViewModels;
+
+namespace WebApp.Application.Services.Interfaces
+{
+	public interface IBasketService
+	{
+		Task<Basket> GetBasket();
+		Task<Basket> UpdateBasket(Basket basket);
+		Task AddItemToBasket(int productId);
+		Task Checkout(BasketDTO basket);
+	}
+}
