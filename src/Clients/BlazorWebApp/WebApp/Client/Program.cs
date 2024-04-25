0321d8943c66e95592a288599e2cf90789e6433b
burakgunce <brk.gnc9@hotmail.com>
2024-04-25 23:24:37 +0300

    nav menu and middleware was updated

--- src/Clients/BlazorWebApp/WebApp/Client/Program.cs
+++ src/Clients/BlazorWebApp/WebApp/Client/Program.cs
@@ -1,6 +1,18 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using WebApp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using WebApp.Application.Services.Interfaces;
+using WebApp.Application.Services;
+using WebApp.Infrastructure;
+using WebApp.Utilities;
 
 namespace WebApp
 {
@@ -10,9 +22,37 @@ namespace WebApp
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredLocalStorage();
+
+
+
+            builder.Services.AddTransient<IIdentityService, IdentityService>();
+            builder.Services.AddTransient<ICatalogService, CatalogService>();
+            builder.Services.AddTransient<IBasketService, BasketService>();
+            builder.Services.AddTransient<IOrderService, OrderService>();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
+            builder.Services.AddSingleton<AppStateManager>();
+
+            builder.Services.AddScoped(sp =>
+            {
+                var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
+
+                return clientFactory.CreateClient("ApiGatewayHttpClient");
+            });
+
+
+            builder.Services.AddScoped<AuthTokenHandler>();
+
+            builder.Services.AddHttpClient("ApiGatewayHttpClient", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:7127/");
+            })
+            .AddHttpMessageHandler<AuthTokenHandler>();
 
             await builder.Build().RunAsync();
         }
