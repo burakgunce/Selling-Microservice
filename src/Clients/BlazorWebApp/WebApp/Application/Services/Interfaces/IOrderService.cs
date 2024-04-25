afef72f87eb27b5407a2ecffd52795dcd536dff3
burakgunce <brk.gnc9@hotmail.com>
2024-04-25 18:14:46 +0300

    order service and checkout page have been added

---
+++ src/Clients/BlazorWebApp/WebApp/Application/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Application.Services.DTOs;
+using WebApp.Domain.Models.ViewModels;
+
+namespace WebApp.Application.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        BasketDTO MapOrderToBasket(Order order);
+    }
+}
